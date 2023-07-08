9bb69b1f6df1b3167187a00edad33f0393549c24
ryanchou-dev <ryanchou111@gmail.com>
2023-07-07 17:22:11 -0700

    i forgor what i did

---
+++ pages/onboarding.tsx
@@ -0,0 +1,38 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import logo from "../public/logo.svg";
+import { FcGoogle } from "react-icons/fc";
+import { db } from "../db/db";
+
+export default async function Home() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  if (!session) {
+    router.push("/");
+    return <></>;
+  } else {
+    // const user = await db.user.find
+    return (
+      <>
+        <div className="px-4 py-2 lg:px-48 lg:py-24">
+          <p className={"inline text-4xl font-bold"}>Login to ChargeIn </p>
+          <Image
+            alt="chargein logo"
+            width="35"
+            className="-mt-2.5 inline "
+            src={logo}
+          />
+
+          <button
+            onClick={() => signIn("google")}
+            className={`font-md mt-6  flex w-max space-x-2 rounded-lg border-2 p-2.5 duration-300 hover:scale-105`}
+          >
+            <FcGoogle className={`h-6 w-6`} /> <span>Sign in with Google</span>
+          </button>
+        </div>
+      </>
+    );
+  }
+}
