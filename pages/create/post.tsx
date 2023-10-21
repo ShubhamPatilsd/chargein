1c85e7ec6f503bd618f02417cccc8b60e4977c32
Shubham <shubhampatilsd@gmail.com>
2023-10-21 13:34:24 -0700

    some changes

--- pages/create/post.tsx
+++ pages/create/post.tsx
@@ -247,7 +247,7 @@ export default function CreatePost() {
                   data: {
                     location: location,
                     kW: energyOutput,
-                    indoor: indoors,
+                    indoor: indoors || true,
                     pricePerHour,
                     teslaOnly,
                     selfCheckIn,
